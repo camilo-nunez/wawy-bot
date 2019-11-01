9003475ca975326f116847f30889941bb69d3496
Camilo Nuñez <camilo.nunezf@sansano.usm.cl>
2019-11-01 20:15:05 -0300

    qweqw

--- index.js
+++ index.js
@@ -3,57 +3,24 @@ const { prefix,token } = require('./config.json');
 
 const client = new Discord.Client();
 
-var isReady = true;
+client.login(token);
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	if(message.content.startsWith(`${prefix}cmd1`)){
-    const channel = message.member.voiceChannel;
-
-    channel.join()
-    .then(connection => console.log('Connected!'))
-    .catch(console.error);
-        
-    // channel.join().then(connection => { // Connection is an instance of VoiceConnection
-    //     console.log("Joined voice channel!");
-    //     message.reply('Waddup');
-    //     let file = path.resolve("./sounds/maledeath4.mp3")
-    //     console.log("File: " + file);
-
-    //     const dispatcher = connection.playFile(path);
-
-    //     let start = 0;
-    //     let end = 0;
-
-    //     dispatcher.on('start', () => {
-    //         start = new Date();
-    //         start = start.getTime();
-    //         connection.player.streamingData.pausedTime = 0;
-    //         dispatcher.setVolume(0.70);
-    //         console.log("Playing");
-
-    //     });
-
-    //     dispatcher.on('debug', (info) => {
-    //         console.log("Debug: " + info);
-    //     });
-
-    //     dispatcher.on('end', reason => {
-    //         console.log("Finished");
-    //         end = new Date();
-    //         end = end.getTime();
-    //         console.log("Playtime: " + (end - start) + "ms");
-    //         console.log("End: " + reason);
-    //     });
-    // });
-        
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('./sounds/maledeath4.mp3');
+            dispatcher.on('finish', () => {
+                console.log('Finished playing!');
+              });
+          } else {
+            message.reply('You need to join a voice channel first!');
+          }
         
     }
-
     
 });
-
-client.login(token);
